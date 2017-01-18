adcb55e9c9aed27c6f23dd8fc28e6c269cc6cadf
Jhonatan Gomes Cavalcanti <jhonatan.gomes93@gmail.com>
2017-01-18 09:45:17 -0200

    Solutions added

--- js/entry.js
+++ js/entry.js
@@ -1,6 +1,7 @@
 require("../css/style.css")
 import {solve1} from "./solution1"
 import {solve8} from "./solution8"
+import {solve13} from "./solution13"
 import {changeButtonStatus} from "./utilities"
 document.write("Links to solutions:")
-export {changeButtonStatus, solve1, solve8}
+export {changeButtonStatus, solve1, solve8, solve13}
---
+++ js/solution14.js
@@ -0,0 +1,10 @@
+import changeButtonStatus from "./utilities"
+
+var longest_chain = 0;
+
+export var solve14 = () => {
+  if(!longest_chain){
+
+  }
+  changeButtonStatus(document.getElementById(""), document.getElementById(), longest_chain);
+}
