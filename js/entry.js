d9ecff67478cbcba3d69d939a91aacf79cde711f
Jhonatan Gomes Cavalcanti <jhonatan.gomes93@gmail.com>
2017-02-24 13:56:25 -0300

    Added workers and removed jquery use

--- js/entry.js
+++ js/entry.js
@@ -1,16 +1,30 @@
 require('./../scss/style.scss')
 
 import {addSolution} from './problemTemplate'
+// import Worker from 'worker-loader!./solvers/solverSolution10'
+// let test = require('worker-loader!./solvers/solverSolution1')()
+
+import work from 'webworkify-webpack'
 
 let solveds = [
-  {id:1, title:'Multiples of 3 and 5'},
-  {id:8, title:'Largest product in a series'},
-  {id:10, title:'Summation of primes'},
-  {id:14, title:'Longest Collatz sequence'},
+  {id:1, title:'Multiples of 3 and 5', worker: work(require.resolve('./solvers/solverSolution1'))},
+  {id:8, title:'Largest product in a series', worker: work(require.resolve('./solvers/solverSolution8'))},
+  {id:10, title:'Summation of primes', worker: work(require.resolve('./solvers/solverSolution10'))},
+  {id:14, title:'Longest Collatz sequence', worker: work(require.resolve('./solvers/solverSolution14'))},
 ]
 
-$(document).ready(() => {
+let fn = function () {
   for (let sol of solveds) {
-    addSolution(sol['id'], sol['title'])
+    addSolution(sol['id'], sol['title'], sol['worker'])
   }
-})
+}
+
+if (document.readyState != 'loading') { // should verify alternative solution
+  fn()
+} else {
+  document.addEventListener('DOMContentLoaded', fn)
+}
+
+// $(document).ready(() => {
+//
+// })
