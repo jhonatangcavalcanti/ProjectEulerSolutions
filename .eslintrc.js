43cd7bf9c881ce9b09da579374acf79e5f3f57ff
Jhonatan Gomes Cavalcanti <jhonatan.gomes93@gmail.com>
2017-02-24 13:53:50 -0300

    Added rule to join path instead of concat string

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,53 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es6": true,
-        "node": true
-    },
-    "globals": {
-        "__dirname": false,
-        "$": false,
-        "describe": false,
-        "it": false,
-        "beforeAll": false,
-        "beforeEach": false,
-        "expect": false,
-        "document": false,
-        "_": false,
-        "postMessage": false,
-        "self": false
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "brace-style": [
-            "error",
-            "1tbs",
-            { "allowSingleLine": true }
-        ],
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ],
-        "no-console":0,
-        "no-var":"error",
-        "keyword-spacing": "error",
-        "space-before-blocks": "error",
-        "arrow-spacing": "error"
-    }
+  "env": {
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  "globals": {
+    "__dirname": false,
+    "$": false,
+    "describe": false,
+    "it": false,
+    "beforeAll": false,
+    "beforeEach": false,
+    "expect": false,
+    "document": false,
+    "_": false,
+    "postMessage": false,
+    "self": false
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "brace-style": [
+      "error",
+      "1tbs",
+      { "allowSingleLine": true }
+    ],
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "no-console":0,
+    "no-var":"error",
+    "keyword-spacing": "error",
+    "space-before-blocks": "error",
+    "arrow-spacing": "error",
+    "no-path-concat": "error"
+  }
 };
